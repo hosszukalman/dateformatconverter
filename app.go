a0c7aa71812ecfc66a63c6ca85919df9e9b76137
Kalman Hosszu <hosszu.kalman@gmail.com>
2014-11-05 15:56:29 +0100

    Create date format constants.

--- app.go
+++ app.go
@@ -1,1 +1,34 @@
 package main
+
+const (
+	FormatLongMonth             = "January"
+	FormatMonth                 = "Jan"
+	FormatNumMonth              = "1"
+	FormatZeroMonth             = "01"
+	FormatLongWeekDay           = "Monday"
+	FormatWeekDay               = "Mon"
+	FormatDay                   = "2"
+	FormatUnderDay              = "_2"
+	FormatZeroDay               = "02"
+	FormatHour                  = "15"
+	FormatHour12                = "3"
+	FormatZeroHour12            = "03"
+	FormatMinute                = "4"
+	FormatZeroMinute            = "04"
+	FormatSecond                = "5"
+	FormatZeroSecond            = "05"
+	FormatLongYear              = "2006"
+	FormatYear                  = "06"
+	FormatPM                    = "PM"
+	Formatpm                    = "pm"
+	FormatTZ                    = "MST"
+	FormatISO8601TZ             = "Z0700" // prints Z for UTC
+	FormatISO8601SecondsTZ      = "Z070000"
+	FormatISO8601ColonTZ        = "Z07:00" // prints Z for UTC
+	FormatISO8601ColonSecondsTZ = "Z07:00:00"
+	FormatNumTZ                 = "-0700" // always numeric
+	FormatNumSecondsTz          = "-070000"
+	FormatNumShortTZ            = "-07"    // always numeric
+	FormatNumColonTZ            = "-07:00" // always numeric
+	FormatNumColonSecondsTZ     = "-07:00:00"
+)
